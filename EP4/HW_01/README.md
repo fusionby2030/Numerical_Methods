82b55ce4b240e9b6d011324f97a6847ee79da3f0
fusionby2030 <adamkit11@gmail.com>
2020-04-22 07:01:21 +0200

    HW01 tutorial prep

---
+++ EP4/HW_01/README.md
@@ -0,0 +1,56 @@
+#HW_01
+
+## Dice Throwing
+
+Anti-Corona team work to see self-organization of the student community in pandemic times.  
+Each of the student subscribed to EP4 should perform the  coin  flip  experiment  and  record  one  single  random  trajectory  of  alengthN= 100.
+That means each student fills in four columns each of length  100.  
+The  first  column A is  simply  the  trial  number i from  1  to100 (Ai=i).  
+The second column B is the result of flipping of the coin you use, either +1 or -1.  
+The third column C is the sum over the second column,  e.g.Ck=∑ki=1Bi.   
+This  is  an  analogue  of  the  displacement.Finally,  the  fourth  column D represents  the  mean  square  displacement D k=∑ki=1(Ci)2
+
+
+
+Afterwards,  you  need  to  find  one  person  who  will perform  an  average  over  the  EP4-ensemble  of  the  results  obtained  and will deliver two graphs:
+
+1. a graph showing all individual displacements (Ci vs.i) and the ensemble-averaged displacement
+
+
+2.  a graph showing all individual mean square displacements (Di vs.i) and the ensemble-averaged MSD.
+
+
+
+
+##Code Comments
+This week we explore the different ways of how to create and manipulate data using the numpy library.
+In the dicesheet.py, one finds the following functions:
+
+###numpy_looping_function
+
+This is a basic and quite understandable way to loop through an initially zero 3D array, and change the values
+throughout the loop.
+
+###vectorized_function
+This function utilizes "vectorization", i.e. efficiently using the entire array as opposed to looping through it
+
+###MSD
+Functions to calculate the mean squared displacement
+
+###Plot creator
+Using matplotlib to make pretty plots of the data
+
+###Main
+One function to rule them all!
+Using this, we can put only the functions we want to use each time we run the program.
+
+##Discussion
+As an output, we receive the time it takes for the two types of data creating functions:
+
+Looping created arrays in 0.0965 seconds
+Vectorizing created arrays in 0.0069 seconds
+Thus vectorizing is much, much more efficient for big data!
+
+In addition, we receive the two graphs:
+![Individual Displacements](/results/IndividualDisplacements.png)
+![MSD](/results/Running_Average_Displacements.png)
--- EP4/HW_01/dicesheet1.py
+++ EP4/HW_01/dicesheet1.py
@@ -1,21 +1,41 @@
+
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import time
 
 
-np.random.seed(420)
+
 
 def initialize_coing_flips():
     return 2*np.random.randint(2, size=(50,100))-1
+
 def flip_coin():
     return 2*np.random.randint(2)-1
 
+
+#Global variables are normally a bad idea!!
 global num_students
 num_students = int(50)
+#Mean Squared Displacements
+
+#1D
+def msd_1d(x):
+    result = np.zeros_like(x)
+    for i in range(1,len(x)):
+        result[i] = np.average((x[i:] - x[:-i])**2)
+    return result
+
+#2D
+def msd_2d(x):
+    result = np.zeros_like(x)
+    for i in range(1, x.shape[1]):
+        result[:, i] = np.average((x[:, i:] - x[:, :-i])**2, axis=1)
+    return result
 
 
-def numpy_function():
+def numpy_looping_function():
     num_trials = int(100)
     data_array = np.zeros((num_students,num_trials,4))
     for n in range(num_students):
@@ -26,79 +46,48 @@ def numpy_function():
             student_array[i][0] = int(i+1)
             coin_flip = flip_coin()
             running_sum += coin_flip
-            running_sum2 += (running_sum**2)/(i+1)
             student_array[i][1] = coin_flip
             student_array[i][2] = running_sum
-            student_array[i][3] = running_sum2
-    #Average for Ensemble
+            student_array[i][3] = 0
+        student_array[:, 3] = msd_1d(student_array[:, 2])
     meaned_array = np.mean(data_array, axis=0)
     return data_array, meaned_array
 
 def vectorized_function():
-    #the trial array can be implemented for teh graphs and is not necessary in the arary storage
-    data_array = np.zeros((50,100,3))
     coin_flips_array = initialize_coing_flips()
-    for i in range(num_students):
-        #df = pd.DataFrame(data=data_array[i], columns=['A','B','C','D'])
-        student_array = data_array[i]
-        student_array[:,0] = coin_flips_array[i]
-        #student_array[:,1] = np.cumsum(coin_flip_array[i])
-        student_array[:,1] = np.cumsum(student_array[:,0])
-        student_array[:,2] = np.cumsum(student_array[:,1])
+    med_dist = np.cumsum(coin_flips_array, axis = 1)
+    print(med_dist.shape)
+    msd_displacement = msd_2d(med_dist)
+    data_array = np.dstack((coin_flips_array, med_dist, msd_displacement))
     meaned_array = np.mean(data_array, axis=0)
     return data_array, meaned_array
 
-
-
-def plot_creator(data_array):
-    """
-    Function to plot the data for a given array.
-    To change for the different graphs, one must change the following variables:
-    plt.title
-    plt.savefig
-    df['C']
-    plt.ylabel
-    And when calling in main be sure to specify which data_array you are graphing
-    """
+def plot_creator(data_array, meaned_array, index, y_axis, graph_name, filename):
     colors = plt.cm.jet(np.linspace(0,1,num_students))
+    trials = np.arange(0, 100)
     for i in range(num_students):
-        df = pd.DataFrame(data=data_array[i], columns=['A','B','C','D'])
-        plt.plot(df['A'], df['C'],  marker='', color=colors[i])
-    plt.title("Individual Displacments")
+        plt.plot(data_array[i, :, index],  marker='', color=colors[i])
+    plt.plot(meaned_array[:, index], color='purple', linewidth=5)
+    plt.legend(handles = [mpatches.Patch(color='purple', label="Ensemble Average")])
+    plt.title(graph_name)
     plt.xlabel("Trials")
-    plt.ylabel("Displacements")
-    plt.savefig('EP4/results/IndividualDisplacements.png')
+    plt.ylabel(y_axis)
+    plt.savefig(filename)
     plt.show()
 
 def main():
     tic = time.perf_counter()
-    data_array, meaned_array = numpy_function()
+    data_array, meaned_array = numpy_looping_function()
 
     toc1 = time.perf_counter()
     vectorized_array, vect_mean = vectorized_function()
     toc2 = time.perf_counter()
-"""
-    print(f"Numpy created arrays in {toc1 - tic:0.4f} seconds")
-    print(f"Vector created arrays in {toc2 - tic:0.4f} seconds")
-    #plot_creator(data_array)
-"""
+
+    print(f"Looping created arrays in {toc1 - tic:0.4f} seconds")
+    print(f"Vectorizing created arrays in {toc2 - toc1:0.4f} seconds")
+    plot_creator(data_array=vectorized_array, meaned_array=vect_mean, index = 1, y_axis="Displacements" ,graph_name='Individual Displacements', filename='results/IndividualDisplacements.png')
+    plot_creator(data_array=vectorized_array, meaned_array = vect_mean, index = 2,  y_axis="Mean_Squared_Displacement" ,graph_name='Running Average Displacements', filename='results/Running_Average_Displacements.png')
+
+
 if __name__ == "__main__":
     main()
-
-
-"""
-for i in range(num_students):
-    df = pd.DataFrame(data=data_array[i], columns=['A','B','C','D'])
-    plt.plot(df['A'], df['D'],  marker='', color=colors[i])
-plt.title("Individual Mean Squared Displacments")
-plt.xlabel("Trials")
-plt.ylabel("MSD")
-plt.show()
-for i in range(num_students):
-    df = pd.DataFrame(data=meaned_array, columns=['A','B','C','D'])
-    plt.plot(df['A'], df['D'],  marker='', color='m')
-plt.title("Ensemble Average  Displacments")
-plt.xlabel("Trials")
-plt.ylabel("Ensemble Average Displacments")
-plt.show()
-"""
