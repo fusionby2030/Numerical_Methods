e8c82c8091802db919320b8554ae9d54b635f2ae
fusionby2030 <adamkit11@gmail.com>
2020-09-30 10:19:16 +0200

    Week Two Update Webinar

---
+++ neuralthesis/meditations/gng/fieldgenerator.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+
+plt.style.use('dark_background')
+pink = '#F08080'
+green = '#96f542'
+gold = '#ebdb34'
+"""
+Infinitly long straight wire in y direction
+"""
---
+++ neuralthesis/meditations/gng/initial_look.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+
+x_values = np.linspace(0, 2*np.pi, num = 200)
+y_values = np.sin(x_values)
